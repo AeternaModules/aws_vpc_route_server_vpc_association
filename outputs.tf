893e76aff97c02eee726038525022d3195112640
Daniel Allen <dan@aeternagroup.net>
2026-08-22 23:38:14 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "vpc_route_server_vpc_associations_id" {
-  description = "Map of id values across all vpc_route_server_vpc_associations, keyed the same as var.vpc_route_server_vpc_associations"
-  value       = { for k, v in aws_vpc_route_server_vpc_association.vpc_route_server_vpc_associations : k => v.id if v.id != null && length(v.id) > 0 }
-}
 output "vpc_route_server_vpc_associations_region" {
   description = "Map of region values across all vpc_route_server_vpc_associations, keyed the same as var.vpc_route_server_vpc_associations"
   value       = { for k, v in aws_vpc_route_server_vpc_association.vpc_route_server_vpc_associations : k => v.region if v.region != null && length(v.region) > 0 }
